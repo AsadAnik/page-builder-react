75a40b754a23cd61f2e09f5798b3cfff775154a5
asadanik <s.engineer63@gmail.com>
2021-06-13 18:11:15 +0600

    Page Builder With GrapesJS & React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import grapesjs from 'grapesjs';
+import gjsPresetWebpage from 'grapesjs-preset-webpage';
+import "./styles/main.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [editor, setEditor] = useState();
+
+    // UseEffect Hook..
+    useEffect(() => {
+        const editor = grapesjs.init({
+            container: '#editor',
+            plugins: [gjsPresetWebpage],
+            pluginsOpts: {
+                gjsPresetWebpage: {},
+            },
+        });
+        setEditor(editor);
+    }, []);
+
+    // returning statement..
+    return (
+       <div className={'App'}>
+           <div id="editor"></div>
+       </div>
+    );
 }
 
 export default App;
