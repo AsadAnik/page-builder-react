dc70d2f63570128dfc2e9aa98b598146a195dd2f
AsadAnik <s.engineer63@gmail.com>
2022-07-13 16:30:27 +0600

    Error Solved

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import grapesjs from 'grapesjs';
 import gjsPresetWebpage from 'grapesjs-preset-webpage';
 import "./styles/main.scss";
+import "grapesjs/src/styles/scss/main.scss";
 
 const App = () => {
     const [editor, setEditor] = useState();
